9ce57a9acae0a5a6fcc47571f5df5e945ed8eb15
George Sun <sunwinner3@gmail.com>
2014-03-03 09:19:48 +0800

    add DP algorithms

---
+++ lib/dp.rb
@@ -0,0 +1,45 @@
+
+module DP
+  # On a positive integer, you can perform any one of the following 3 steps.
+  #   1.) Subtract 1 from it. (n = n-1),
+  #   2.) If its divisible by 2, divide by 2. (if n % 2 == 0, then n = n/2),
+  #   3.) If its divisible by 3, divide by 3. (if n % 3 == 0, then n = n/3).
+  # Now the question is, given a positive integer n, find the minimum number of steps that takes n to 1
+  # eg: 1.) For n = 1, output: 0
+  #     2.) For n = 4, output: 2  (4/2 = 2/2 = 1)
+  #     3.) For n = 7, output: 3  (7-1 = 6/3 = 2/2 = 1)
+  MEMO = []
+  def self.get_min_steps(n)
+    raise ArgumentError if n < 1
+    # base case
+    return 0 if n == 1
+    return MEMO[n] if MEMO[n]
+    steps = 1 + get_min_steps(n - 1)
+    steps = [steps, 1 + get_min_steps(n / 2)].min if (n % 2 == 0)
+    steps = [steps, 1 + get_min_steps(n / 3)].min if (n % 3 == 0)
+    MEMO[n] = steps
+    steps
+  end
+
+  # the Longest Increasing Subsequence problem is to find the
+  # longest increasing subsequence of a given sequence.
+  # TODO not finish yet.
+  def self.longest_increase_subseq(ary)
+    raise ArgumentError unless ary
+    seq = Array.new(ary.size)
+    0.upto(ary.size - 1) do |i|
+      seq[i] = 1
+      0.upto(ary.size - 1) do |j|
+        if ary[i] > ary[j] and seq[i] < seq[j]
+          seq[i] = seq[j] + 1
+        end
+      end
+    end
+    0.upto(seq.size - 2) do |i|
+      if seq[i] == 1
+        l = i, r = 0, largest = 0
+      end
+      largest = seq[i] if largest < seq[i]
+    end
+  end
+end
---
+++ spec/dp/dp_spec.rb
@@ -0,0 +1,37 @@
+
+require_relative '../spec_helper'
+require_relative '../../lib/dp'
+
+describe "Dynamic Programming Problems" do
+  describe "Minimum steps to one" do
+    it "should raise ArgumentError if n is lower then 1" do
+      expect{DP::get_min_steps(0)}.to raise_error(ArgumentError)
+    end
+
+    it "should output 0 if n is 1" do
+      DP::get_min_steps(1).should == 0
+    end
+
+    it "should output 2 if n is 4" do
+      DP::get_min_steps(4).should == 2
+    end
+
+    it "should output 3 if n is 7" do
+      DP::get_min_steps(7).should == 3
+    end
+  end
+
+  describe "Longest Increasing Subsequence" do
+    before do
+      @ary = [8, 7, 1, 3, 5, 9, 10, 2, 4, 6]
+    end
+
+    it "should raise ArgumentError if passed in nil" do
+      expect {DP::longest_increase_subseq(nil)}.to raise_error(ArgumentError)
+    end
+
+    it "should find the longest subsequence" do
+      DP::longest_increase_subseq(@ary).should eql([1, 2, 3, 9, 10])
+    end
+  end
+end
