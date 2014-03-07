a9212954c3411c1c662419732857da8de7479c87
George Sun <gsun@successfactors.com>
2014-03-07 16:47:06 +0800

    New linked list related algorithms.

--- lib/lists.rb
+++ lib/lists.rb
@@ -17,6 +17,70 @@ module Ch2
     end
   end
 
+  # find the kth to the last element of a singly linked list
+  # there's no size of the linked list information stored.
+  def self.last_kth_recursive(node, k, result)
+    raise ArgumentError if k <= 0
+    return 0 if node.nil?
+    index = last_kth_recursive(node.next, k, result) + 1
+    puts node.value if k == index
+    index
+  end
+
+  # Traverse the singly linked list with two iteraters
+  def self.last_kth_iterative(node, k)
+    raise ArgumentError unless node && k > 0
+    p1 = node; p2 = node
+    k.times do
+      return nil if p2.nil?
+      p2 = p2.next
+    end
+    return nil if p2.nil?
+    until p2.next.nil?
+      p1 = p1.next
+      p2 = p2.next
+    end
+    p1.value
+  end
+
+  # Implement an algorithm to delete a node in the
+  # middle of a singuly linked list.
+  # given only access to that node
+  def self.delete_node(node)
+    # to achieve this, we need to copy next node to
+    # current node, then delete next node instead
+    return nil if node.nil? or node.next.nil?
+    node.value = node.next.value
+    node.next = node.next.next
+  end
+
+  # write code to partition a linked list around a value x,
+  # such that all nodes less than x come before
+  # all nodes greater than or equal x
+  def self.partition(node, key)
+    before_start = after_start = nil
+    until node.nil?
+      n = node.next
+      break if n.nil?
+      if n.value < key
+        n.next = before_start
+        before_start = n
+      else
+        n.next = after_start
+        after_start = n
+      end
+      node = n
+    end
+
+    return after_start if before_start.nil?
+    head = before_start
+    unless before_start.next.nil?
+      before_start = before_start.next
+    end
+    before_start.next = after_start
+    head
+  end
+
   class Node
     attr_accessor :value, :next
 
--- spec/ch2/lists_spec.rb
+++ spec/ch2/lists_spec.rb
@@ -21,4 +21,41 @@ describe "Linked list related algorithms" do
       Ch2::circular?(nil).should == false
     end
   end
-end
+
+  describe "find the kthto last element of a singly linked list" do
+    before do
+      @list = Ch2::LinkedList.new.add("a").add("b").add("c").add("d")
+    end
+
+    it "should raise error if passed in illegal arguments" do
+      expect {Ch2::last_kth_iterative(@list, -1)}.to raise_error(ArgumentError)
+      expect {Ch2::last_kth_recursive(@list, -1, "")}.to raise_error(ArgumentError)
+    end
+
+    it "should return correct result" do
+      Ch2::last_kth_iterative(@list.head, 2).should eql("b")
+    end
+  end
+
+  describe "remove current node without access to head" do
+    before do
+      @list = Ch2::LinkedList.new.add("a").add("b").add("c")
+    end
+
+    it "should return nil if node not exist" do
+      Ch2::delete_node(nil).should be_nil
+    end
+
+    describe "remove the node" do
+      before do
+        Ch2::delete_node(@list.head.next)
+      end
+
+      it "should remove correct node" do
+        @list.head.value.should eql("a")
+        @list.head.next.value.should eql("c")
+        @list.head.next.next.should be_nil
+      end
+    end
+  end
+ end
